19e7fd28b112208493dbcd010ec4a138f396619e
ozudev <ozudev@gmail.com>
2024-12-16 19:17:00 +0300

    Update Week1

--- Week1/Program.cs
+++ Week1/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Oguzhan Uguz \nFullstack Developer olma yolunda ilerleyebilmek adina Patika'dayim");
