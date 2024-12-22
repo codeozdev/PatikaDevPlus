44045922b89601ec7770feb614e8283f89654865
ozudev <ozudev@gmail.com>
2024-12-22 06:20:12 +0300

    week 2 uygulama 1

---
+++ Week2_01/Program.cs
@@ -0,0 +1,66 @@
+﻿// 1 -> Konsol ekranına 10 kere "Kendime inanıyorum, ben bu yazılım işini hallederim!" yazdırınız.
+
+for (var i = 0; i < 10; i++)
+{
+    Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+}
+
+Console.WriteLine("*******************************************************************************************");
+
+// 2 -> 1 ile 20 arasındaki sayıları konsol ekranına yazdırınız.
+
+for (var i = 1; i <= 20; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("*******************************************************************************************");
+
+
+// 3 -> 1 ile 20 arasındaki çift sayıları konsol ekranına yazdırınız.
+
+
+for (var i = 1; i <= 20; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+Console.WriteLine("*******************************************************************************************");
+
+// 4-> 50 ile 150 arasındaki sayıların toplamını ekrana yazdırınız.
+
+var sum = 0;
+
+for (var i = 50; i <= 150; i++)
+{
+    sum += i;
+}
+
+
+Console.WriteLine(sum);
+
+
+Console.WriteLine("*******************************************************************************************");
+
+// 5 -> 1 ile 120 arasındaki tek ve çift sayıların toplamlarını ayrı ayrı ekrana yazdırınız.
+
+var sumEven = 0;
+var sumOdd = 0;
+
+for (var i = 1; i <= 120; i++)
+{
+    if (i % 2 == 0)
+    {
+        sumEven += i;
+    }
+    else
+    {
+        sumOdd += i;
+    }
+}
+
+Console.WriteLine($"Çift sayıların toplamı: {sumEven}");
+Console.WriteLine($"Tek sayıların toplamı: {sumOdd}");
